797b7932035d34ed2e90a7b8433a0019bb7406d1
Fazly <shabnamfazly@gmail.com>
2023-08-02 20:57:00 +0100

    added get routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,11 +4,20 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include:[Product],
+  }).then((categories)=>res.json(categories)).catch((error)=>res.status(500).json(error))
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where:{id:req.params.id},
+    include:[Product]
+  })
+    .then((Category)=>res.json(Category)).catch((error)=>res.status(500).json(error))
+ 
   // find one category by its `id` value
   // be sure to include its associated Products
 });
